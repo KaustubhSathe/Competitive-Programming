11084a2a36b59e6cefc019d28c92685cbef6171e
kaustubhofb <kaustubh.sathe@ofbusiness.in>
2019-11-29 13:57:54 +0530

    change

--- interview2020.cpp
+++ interview2020.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*********************************************************************** INTERVIEW DSA PROBLEMS *************************************************************************
 // Given arrival and departure times of all trains that reach a railway station.
 // Your task is to find the minimum number of platforms required for the railway station so that no train waits.
 // https://practice.geeksforgeeks.org/problems/minimum-platforms/0
@@ -31,104 +32,6 @@ using namespace std;
 // 	}
 // 	return 0;
 // }
-
-// Minimum number of bracket reversals needed to make an expression balanced
-// https://practice.geeksforgeeks.org/problems/count-the-reversals/0
-//https://www.geeksforgeeks.org/minimum-number-of-bracket-reversals-needed-to-make-an-expression-balanced/
-// int main(){
-//     int tc;
-//     cin>>tc;
-//     while(tc--){
-//         string s;cin>>s;
-//         if(s.length()%2 == 1){
-//             cout<<-1<<endl;
-//             continue;
-//         }
-//         stack<char> pq;
-//         for(auto x : s){
-//             if(pq.empty()){
-//                 pq.push(x);
-//             }else{
-//                 if(pq.top() == '{' && x == '}'){
-//                     pq.pop();
-//                 }else{
-//                     pq.push(x);
-//                 }
-//             }
-//         }
-//         int cnt1 = 0,cnt2 = 0;
-//         if(cnt1 != cnt2){
-//             cout<<-1<<endl;
-//             continue;
-//         }
-//         while(!pq.empty()){
-//             cnt1 += (pq.top() == '{');
-//             cnt2 += (pq.top() == '}');
-//             pq.pop();
-//         }
-        
-//         int ans = (int)ceil(cnt1/2.0) + (int)ceil(cnt2/2.0);
-//         cout<<ans<<endl;
-//     }
-//     return 0;
-// }
-
-// merge (initer1 beg1, initer1 end1,initer2 beg2, initer2 end2,outiter res, Compare comp)
-// Use back_inserter merge(temp1.begin(),temp1.end(),temp2.begin(),temp2.end(),back_inserter(temp3))
-
-// /********************Min-Heap********************/
-// template<class T> struct Heap{
-//     vector<T> table;
-//     /**********Constructors**********/
-//     Heap(){}
-//     Heap(vector<T> input){
-//         for(auto x : input){
-//             insert(x);
-//         }
-//     }
-
-//     /**********Methods**********/
-//     void insert(T x){
-//         table.push_back(x);
-//         for(int i = table.size()-1;i != 0 && table[(i-1)/2] > table[i];){
-//             swap(table[(i-1)/2],table[i]);
-//             i = (i-1)/2;
-//         }
-//     }
-
-//     T extractMin(){
-//         if(table.size() == 0)
-//             return 0;
-        
-
-//         T ret = table.front();
-//         swap(table[table.size() - 1],table[0]);
-//         table.erase(table.begin() + table.size() - 1);
-//         Heapify(0);
-//         return ret;
-//     }
-
-//     T getMin(){
-//         return table.front();
-//     }
-
-//     void Heapify(int id){
-//         int left = 2*id + 1;
-//         int right = 2*id + 2;
-//         int smallest = id;
-//         if(left < table.size() && table[left] < table[smallest])
-//             smallest = left;
-
-//         if(right < table.size() && table[right] < table[smallest])
-//             smallest = right;
-
-//         if(smallest != id){
-//             swap(table[smallest],table[id]);
-//             Heapify(smallest);
-//         }
-//     }
-// };
-
 // https://www.geeksforgeeks.org/merge-k-sorted-arrays/
 
 // https://leetcode.com/problems/lru-cache/
@@ -173,7 +76,102 @@ using namespace std;
 //     }
 // };
 
-/************************************************************************* Interfaces *****************************************************************************
+
+// Minimum number of bracket reversals needed to make an expression balanced
+// https://practice.geeksforgeeks.org/problems/count-the-reversals/0
+//https://www.geeksforgeeks.org/minimum-number-of-bracket-reversals-needed-to-make-an-expression-balanced/
+// int main(){
+//     int tc;
+//     cin>>tc;
+//     while(tc--){
+//         string s;cin>>s;
+//         if(s.length()%2 == 1){
+//             cout<<-1<<endl;
+//             continue;
+//         }
+//         stack<char> pq;
+//         for(auto x : s){
+//             if(pq.empty()){
+//                 pq.push(x);
+//             }else{
+//                 if(pq.top() == '{' && x == '}'){
+//                     pq.pop();
+//                 }else{
+//                     pq.push(x);
+//                 }
+//             }
+//         }
+//         int cnt1 = 0,cnt2 = 0;
+//         if(cnt1 != cnt2){
+//             cout<<-1<<endl;
+//             continue;
+//         }
+//         while(!pq.empty()){
+//             cnt1 += (pq.top() == '{');
+//             cnt2 += (pq.top() == '}');
+//             pq.pop();
+//         }
+        
+//         int ans = (int)ceil(cnt1/2.0) + (int)ceil(cnt2/2.0);
+//         cout<<ans<<endl;
+//     }
+//     return 0;
+// }
+*********************************************************************** INTERVIEW DSA PROBLEMS *************************************************************************/
+
+/****************************************************************** DATA STRUCTURES IMPLEMENTATIONS ***************************************************************
+
+template<class T> struct Heap{
+    vector<T> table;
+    Heap(){}
+    Heap(vector<T> input){
+        for(auto x : input){
+            insert(x);
+        }
+    }
+    void insert(T x){
+        table.push_back(x);
+        for(int i = table.size()-1;i != 0 && table[(i-1)/2] > table[i];){
+            swap(table[(i-1)/2],table[i]);
+            i = (i-1)/2;
+        }
+    }
+    T extractMin(){
+        if(table.size() == 0)
+            return 0;
+        
+
+        T ret = table.front();
+        swap(table[table.size() - 1],table[0]);
+        table.erase(table.begin() + table.size() - 1);
+        Heapify(0);
+        return ret;
+    }
+
+    T getMin(){
+        return table.front();
+    }
+
+    void Heapify(int id){
+        int left = 2*id + 1;
+        int right = 2*id + 2;
+        int smallest = id;
+        if(left < table.size() && table[left] < table[smallest])
+            smallest = left;
+
+        if(right < table.size() && table[right] < table[smallest])
+            smallest = right;
+
+        if(smallest != id){
+            swap(table[smallest],table[id]);
+            Heapify(smallest);
+        }
+    }
+};
+
+****************************************************************** DATA STRUCTURES IMPLEMENTATIONS ***************************************************************/
+
+/************************************************************************* INTERFACES *****************************************************************************
 Interface : Like a class, an interface can have methods and variables, but the methods declared in an interface are by default abstract.
  - Interfaces specify what a class must do and not how. It is the blueprint of the class.
  - If a class implements an interface and does not provide method bodies for all functions specified in the interface, then the class must be declared abstract.0
@@ -228,9 +226,29 @@ Interface : Like a class, an interface can have methods and variables, but the m
  - All the methods are public and abstract. And all the fields are public, static, and final.
  - It is used to achieve multiple inheritance.
  - It is used to achieve loose coupling.
-
- 
 ************************************************************************* Interfaces ******************************************************************************/
 
+/*********************************************************************** IMPORTANT STL ****************************************************************************
+Policy Based Data Structure
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> indexed_set;
+order_of_key (val): returns the no. of values less than val
+find_by_order (k): returns the iterator to kth largest element.(0-based)
 
 
+
+
+
+*********************************************************************** IMPORTANT STL ****************************************************************************/
+
+
+
+
+/********************************************************************** CODE - HERE ******************************************************************************/
+int main(){
+
+}
