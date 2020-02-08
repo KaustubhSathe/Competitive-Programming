4c4e6924b77ff311655167772fdbfd5abdae1267
KaustubhSathe <f2015466@pilani.bits-pilani.ac.in>
2020-02-08 12:00:20 +0530

    change

--- my_library/DSU on Trees.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-
-
-/* Using DSU on trees we can answer this type of queries, how many vertices in the subtree of vertex v 
-has some property in O(nlogn) for all the queries.
-
-*/
